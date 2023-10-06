1d2dafbb0f2959a62646bb352ca4956894a3ec82
vladysl4v <t1ton7@yahoo.com>
2023-10-06 15:54:01 +0300

    Add swagger authorization button

--- WebTimetable.Api/Program.cs
+++ WebTimetable.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 
+using WebTimetable.Api;
 using WebTimetable.Application;
 
 
@@ -31,39 +32,7 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddApplication(builder.Environment.IsDevelopment());
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "SmartTimetable API",
-        Description = "Web API for schedule-based projects.",
-        Contact = new OpenApiContact
-        {
-            Name = "Vladyslav Arkhypenkov",
-            Email = "ArchipenkovV@krok.edu.ua",
-            Url = new Uri("https://github.com/vladysl4v/"),
-        },
-        License = new OpenApiLicense
-        {
-            Name = "The GNU General Public License v3.0",
-            Url = new Uri("https://www.gnu.org/licenses/gpl-3.0.html#license-text")
-        }
-    });
-    // Activate swagger controllers documentation
-    options.CustomSchemaIds(type => type.ToString());
-
-    var currentAssembly = System.Reflection.Assembly.GetExecutingAssembly();
-    var xmlDocs = currentAssembly.GetReferencedAssemblies()
-        .Union(new[] { currentAssembly.GetName() })
-        .Select(a => Path.Combine(Path.GetDirectoryName(currentAssembly.Location)!, $"{a.Name}.xml"))
-        .Where(File.Exists).ToArray();
-
-    foreach (var doc in xmlDocs)
-    {
-        options.IncludeXmlComments(doc);
-    }
-});
+builder.Services.AddSwaggerConfiguration();
 
 var app = builder.Build();
 
---
+++ WebTimetable.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+namespace WebTimetable.Api;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        var apiInformation = new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "SmartTimetable API",
+            Description = "Web API for schedule-based projects.",
+            Contact = new OpenApiContact
+            {
+                Name = "Vladyslav Arkhypenkov",
+                Email = "ArchipenkovV@krok.edu.ua",
+                Url = new Uri("https://github.com/vladysl4v/"),
+            },
+            License = new OpenApiLicense
+            {
+                Name = "The GNU General Public License v3.0",
+                Url = new Uri("https://www.gnu.org/licenses/gpl-3.0.html#license-text")
+            }
+        };
+
+        var securityScheme = new OpenApiSecurityScheme
+        {
+            BearerFormat = "Microsoft access token",
+            Description = "Insert your microsoft access token",
+            Name = "JWT Authentication",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = JwtBearerDefaults.AuthenticationScheme,
+            Reference = new OpenApiReference
+            {
+                Id = JwtBearerDefaults.AuthenticationScheme,
+                Type = ReferenceType.SecurityScheme
+            }
+        };
+        services.AddSwaggerGen(options =>
+        {
+            // Configure common information about API
+            options.SwaggerDoc("v1", apiInformation);
+
+            // Configure authorization button
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                { securityScheme, new List<string>() }
+            });
+
+            // Activate swagger controllers documentation
+            options.CustomSchemaIds(type => type.ToString());
+
+            var currentAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var xmlDocs = currentAssembly.GetReferencedAssemblies()
+                .Union(new[] { currentAssembly.GetName() })
+                .Select(a => Path.Combine(Path.GetDirectoryName(currentAssembly.Location)!, $"{a.Name}.xml"))
+                .Where(File.Exists).ToArray();
+
+            foreach (var doc in xmlDocs)
+            {
+                options.IncludeXmlComments(doc);
+            }
+        });
+
+        return services;
+    }
+}
